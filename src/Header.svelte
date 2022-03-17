3558a6219ca597f3e845293939b4e7ff172b753d
Jonas Holbech <jh@jonasholbech.dk>
2022-03-17 21:11:48 +0100

    fix hover glitch in ff/chrome

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,161 +1,135 @@
 <script>
-import { tick } from 'svelte';
+  let paused = true;
+  let time;
 
-
-let paused = true;
-let time;
-
-function mouseOver (){
+  function mouseOver() {
     paused = false;
-}
+  }
 
-// function mouseOut (){
-//     paused = true;
-//     time = 0;
-// }
-
-async function mouseOut(){
-
-await tick()
-
-time=0;
-paused=true;
-
-
-}
-
+  function mouseOut() {
+    setTimeout(() => {
+      time = 0;
+    }, 16);
+    paused = true;
+  }
 </script>
 
 <section class="section_wrapper">
-<video muted loop id="myVideo"
-       src="./glitch.mp4"
-       bind:paused={paused}
-       bind:currentTime={time}
-       >
-</video>
+  <video
+    muted
+    loop
+    id="myVideo"
+    src="./glitch.mp4"
+    bind:paused
+    bind:currentTime={time}
+  />
 
+  <h1
+    on:focus
+    on:pointerenter={mouseOver}
+    on:blur
+    on:pointerleave={mouseOut}
+    id="myBtn"
+    class="heading"
+  >
+    ANNE MY - MULTIMEDIA/DIGITAL DESIGNER
+  </h1>
 
-<h1 on:focus on:pointerenter={mouseOver} on:blur on:pointerleave={mouseOut} id="myBtn" class="heading">ANNE MY - MULTIMEDIA/DIGITAL DESIGNER</h1>
-
-<div class="pil">
-<img src="./down-arrow.png" alt="" >
-</div>
-
+  <div class="pil">
+    <img src="./down-arrow.png" alt="" />
+  </div>
 </section>
 
 <style>
-
-.section_wrapper {
+  .section_wrapper {
     display: grid;
-    
-}
+  }
 
-#myVideo, .heading, .pil {
+  #myVideo,
+  .heading,
+  .pil {
     grid-area: 1/1;
-    
-}
+  }
 
-.heading {
+  .heading {
     /* margin-left: 2rem; */
     font-size: medium;
-}
+  }
 
-#myVideo {
-   height: 844px;
-   width: 390px;
-   object-fit: cover;
+  #myVideo {
+    height: 844px;
+    width: 390px;
+    object-fit: cover;
     object-position: center center;
     z-index: -1;
-   
-}
+  }
 
-.pil {
+  .pil {
     place-self: end center;
     margin-left: 1rem;
-    
-}
+  }
 
-img {
+  img {
     width: 15%;
     padding-left: 10rem;
-}
+  }
 
-
-.heading {
+  .heading {
     place-self: center center;
     color: white;
-}
+  }
 
-
-
-#myBtn:hover {
+  #myBtn:hover {
     /* color: var(--primary-hover-color); */
-    color:var(--primary-hover-color);
-}
+    color: var(--primary-hover-color);
+  }
 
+  /* mediaquerys */
 
-/* mediaquerys */
-
-@media (min-width: 640px) {
-
+  @media (min-width: 640px) {
     #myVideo {
-   /* height: 1080px; */
-    width: 640px;
-    height: 26.75rem;
-    height: clamp(26.75rem, 16.287921348314605rem + 42.921348314606746vw, 50.625rem);
-   
-}
-
-    .pil {
-        margin-left: 8rem;
-
-}
-
-
-
-}
-
-@media (min-width: 768px){
-
-    #myVideo {
-   /* height: 1080px; */
-   width: 810px;
-   
-}
-.pil {
-    margin-left: 13rem;
-    
-}
-
-}
-
-
-
-@media (min-width: 1024px) {
-    .heading {
-        font-size: xx-large;
+      /* height: 1080px; */
+      width: 640px;
+      height: 26.75rem;
+      height: clamp(
+        26.75rem,
+        16.287921348314605rem + 42.921348314606746vw,
+        50.625rem
+      );
     }
 
     .pil {
-    margin-left: 15rem;
-    
-}
+      margin-left: 8rem;
+    }
+  }
 
-img {
-    width: 20%;
-    padding-left: none;
-}
+  @media (min-width: 768px) {
+    #myVideo {
+      /* height: 1080px; */
+      width: 810px;
+    }
+    .pil {
+      margin-left: 13rem;
+    }
+  }
 
+  @media (min-width: 1024px) {
+    .heading {
+      font-size: xx-large;
+    }
 
-#myVideo {
-    width: 100%;
-    /* height: auto; */
-}
+    .pil {
+      margin-left: 15rem;
+    }
 
-}
+    img {
+      width: 20%;
+      padding-left: none;
+    }
 
-
-
+    #myVideo {
+      width: 100%;
+      /* height: auto; */
+    }
+  }
 </style>
-
-
